adedfe67d5e3467481e2fadd34878bbea681965b
Matthew Cairns <mattrcairns@gmail.com>
2019-04-02 19:30:59 -0700

    Fixed indent err

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def home():
     render_bus_locations('templates/index.html')
     return render_template('index.html')
 
- if __name__ == '__main__':
+if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
