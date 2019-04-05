3efefda21756ce3f5f09203060db70dc9962181c
Matthew Cairns <mattrcairns@gmail.com>
2019-04-05 11:30:14 -0700

    Now returns an array with bus information

--- bus_mapper.py
+++ bus_mapper.py
@@ -11,7 +11,7 @@ def get_realtime_data(link):
     return feed
 
 
-def render_bus_locations(file_name):
+def get_bus_locations():
     colors = ['red', 'blue', 'green', 'purple', 'orange', 'darkred',
                  'lightred', 'darkblue', 'darkgreen', 'cadetblue',
                  'darkpurple', 'lightblue', 'lightgreen',
@@ -27,26 +27,28 @@ def render_bus_locations(file_name):
     routes_static = read_routes('data/routes_static.txt')
     shapes = read_shapes('data/shapes.txt')
 
-    m = folium.Map(
-        location=[48.427502, -123.367264],
-        zoom_start=12,
-        tiles='Stamen Terrain'
-    )
-
-
-
+    bus_locations = []
     for bus in feed.entity:
         trip_id = bus.vehicle.trip.trip_id
         if trip_id != '' and trip_id in trips:
             bus_name = trips[trip_id][3]
             route_id = trips_static[bus_name][0]
             route_short_name = routes_static[route_id][2]
-            c = random.choice(colors)
-            folium.Marker([bus.vehicle.position.latitude, bus.vehicle.position.longitude],
-                          popup=route_short_name + ' ' + bus_name, icon=folium.Icon(
-                          color=c, angle = int(bus.vehicle.position.bearing), icon='arrow-up')
-                          ).add_to(m)
-            folium.PolyLine(shapes[trips[trip_id][7]], color=c).add_to(m)
+            bus_locations.append([bus.vehicle.position.latitude, bus.vehicle.position.longitude,
+                                    bus.vehicle.position.bearing])
+    #         c = random.choice(colors)
+    #         folium.Marker([bus.vehicle.position.latitude, bus.vehicle.position.longitude],
+    #                       popup=route_short_name + ' ' + bus_name, icon=folium.Icon(
+    #                       color=c, angle = int(bus.vehicle.position.bearing), icon='arrow-up')
+    #                       ).add_to(m)
+    #         folium.PolyLine(shapes[trips[trip_id][7]], color=c).add_to(m)
 
 
-    m.save(file_name)
+    return bus_locations
+
+def main():
+    render_bus_locations('templates/index.html')
+
+
+if __name__ == '__main__':
+    main()
